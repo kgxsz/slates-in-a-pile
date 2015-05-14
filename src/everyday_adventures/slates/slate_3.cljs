13d793b5a6e516d8961246121981be2dcfe9a53e
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-05-14 21:33:48 +0100

    Major refactorings

--- src/everyday_adventures/slates/slate_3.cljs
+++ src/everyday_adventures/slates/slate_3.cljs
@@ -3,9 +3,6 @@
             [om-tools.core :refer-macros [defcomponent]]
             [om-tools.dom :refer [div h1 p]]))
 
-#_(circle (-> svg (.selectAll "circle") (.data #js [3 2 1])))
-#_(-> circle .enter (.append "circle") (.attr "r" 7) (.attr "cy" 214) (.attr "cx" (fn [d i] (+ 330 (* 300 (+ i step))))))
-
 (defn create-svg []
   (-> js/d3 (.select "#slate-3 .slate-content")
             (.append "svg")
@@ -15,103 +12,44 @@
 (defn use-attribute [element attribute]
   (.attr element attribute (fn [d _] (aget d attribute))))
 
-(defn use-attributes [element & attributes]
-  (reduce use-attribute element attributes))
+(defn create-element-join [parent-element element-name data]
+    (let [join (-> parent-element (.selectAll element-name) (.data (clj->js data)))
+          attributes (->> data first keys (mapv name))
+          use-attributes (fn [element attributes] (reduce use-attribute element attributes))]
+      (-> join .enter (.append element-name) (use-attributes attributes))))
 
-#_(defn enter-with-attributes [container element data]
-  (let [t (-> container (.selectAll element) (.data (clj->js data)) .enter (.append element))]
-    ))
+(defn create-text-join [parent-element data]
+    (let [join (-> parent-element (.selectAll "text") (.data (clj->js data)))
+          use-text (fn [element] (.text element (fn [d _] (aget d "text"))))]
+      (-> join .enter (.append "text") (use-attribute "dx") (use-attribute "dy") (use-text))))
 
-(defn create-service-line [svg]
-  (let [g (-> svg (.append "g") (.attr "transform" "translate(0, 200)") (.attr "class" "service-line"))]
-    #_(enter-with-attributes g "rect" [{:width 100 :height 28 :x 5 :rx 10 :ry 10}])
-    (-> g (.selectAll "rect") (.data (clj->js [{:width 100 :height 28 :x 5 :rx 10 :ry 10}]))
-          .enter (.append "rect") (use-attributes "width" "height" "x" "rx" "ry"))
-    (-> g (.selectAll "text") (.data #js ["service a"]) .enter
-                    (.append "text") (.text (fn [d i] d)) (.attr "dy" 19) (.attr "dx" 24))
-    (-> g (.selectAll "line")
-                  (.data (clj->js [{:x1 105 :x2 320 :y1 14 :y2 14 :class "dashed"}
+(defn create-element-grouping [parent-element class x y]
+  (let [transform (str "translate(" x "," y ")")]
+    (-> parent-element (.append "g") (.attr "class" class) (.attr "transform" transform))))
+
+(defn create-service-static [parent-element service y-offset]
+  (let [g (create-element-grouping parent-element "service" 0 y-offset)]
+    (create-element-join g "rect" [{:width 100 :height 28 :x 5 :rx 10 :ry 10}])
+    (create-text-join g [{:text service :dx 24 :dy 19}])
+    (create-element-join g "line" [{:x1 105 :x2 320 :y1 14 :y2 14 :class "dashed"}
                                    {:x1 340 :x2 620 :y1 14 :y2 14 :class "solid"}
-                                   {:x1 640 :x2 920 :y1 14 :y2 14 :class "solid"}]))
-                  .enter (.append "line")
-                  (.attr "x1" (fn [d _] (.-x1 d)))
-                  (.attr "x2" (fn [d _] (.-x2 d)))
-                  (.attr "y1" (fn [d _] (.-y1 d)))
-                  (.attr "y2" (fn [d _] (.-y2 d)))
-                  (.attr "class" (fn [d _] (.-class d))))))
+                                   {:x1 640 :x2 920 :y1 14 :y2 14 :class "solid"}])))
 
-(defn create-dev-line [svg]
-  (let [g (-> svg (.append "g") (.attr "transform" "translate(300, 100)") (.attr "class" "dev-line"))]
-    (-> g (.selectAll "circle") (.data (clj->js [{:cx 30 :cy 10 :r 24}]))
-          .enter (.append "circle")
-          (.attr "cx" (fn [d _] (.-cx d)))
-          (.attr "cy" (fn [d _] (.-cy d)))
-          (.attr "r" (fn [d _] (.-r d))))
-    #_(-> g (.selectAll "polygon") (.data #js [1]) .enter (.append "polygon")
-        (.attr "points" "10,0 40,0 25,25") (.attr "style" "fill:#343434"))
-    (-> g (.selectAll "text") (.data #js ["dev"]) .enter
-                    (.append "text") (.text (fn [d i] d)) (.attr "dy" 15) (.attr "dx" 30))
-    (-> g (.selectAll "line")
-                  (.data (clj->js [{:x1 30 :x2 30 :y1 34 :y2 103 :class "solid"} {:x1 30 :x2 30 :y1 125 :y2 153 :class "solid"}]))
-                  .enter (.append "line")
-                  (.attr "x1" (fn [d _] (.-x1 d)))
-                  (.attr "x2" (fn [d _] (.-x2 d)))
-                  (.attr "y1" (fn [d _] (.-y1 d)))
-                  (.attr "y2" (fn [d _] (.-y2 d)))
-                  (.attr "class" (fn [d _] (.-class d))))))
-
-(defn create-qa-line [svg]
-  (let [g (-> svg (.append "g") (.attr "transform" "translate(600, 100)") (.attr "class" "qa-line"))]
-    (-> g (.selectAll "circle") (.data (clj->js [{:cx 30 :cy 10 :r 24}]))
-          .enter (.append "circle")
-          (.attr "cx" (fn [d _] (.-cx d)))
-          (.attr "cy" (fn [d _] (.-cy d)))
-          (.attr "r" (fn [d _] (.-r d))))
-    #_(-> g (.selectAll "polygon") (.data #js [1]) .enter (.append "polygon")
-        (.attr "points" "10,0 40,0 25,25") (.attr "style" "fill:#343434"))
-    (-> g (.selectAll "text") (.data #js ["qa"]) .enter
-                    (.append "text") (.text (fn [d i] d)) (.attr "dy" 15) (.attr "dx" 30))
-    (-> g (.selectAll "line")
-                  (.data (clj->js [{:x1 30 :x2 30 :y1 34 :y2 103 :class "solid"} {:x1 30 :x2 30 :y1 125 :y2 153 :class "solid"}]))
-                  .enter (.append "line")
-                  (.attr "x1" (fn [d _] (.-x1 d)))
-                  (.attr "x2" (fn [d _] (.-x2 d)))
-                  (.attr "y1" (fn [d _] (.-y1 d)))
-                  (.attr "y2" (fn [d _] (.-y2 d)))
-                  (.attr "class" (fn [d _] (.-class d))))))
-
-(defn create-prod-line [svg]
-  (let [g (-> svg (.append "g") (.attr "transform" "translate(900, 100)") (.attr "class" "prod-line"))]
-    (-> g (.selectAll "circle") (.data (clj->js [{:cx 30 :cy 10 :r 24}]))
-          .enter (.append "circle")
-          (.attr "cx" (fn [d _] (.-cx d)))
-          (.attr "cy" (fn [d _] (.-cy d)))
-          (.attr "r" (fn [d _] (.-r d))))
-    #_(-> g (.selectAll "polygon") (.data #js [1]) .enter (.append "polygon")
-        (.attr "points" "10,0 40,0 25,25") (.attr "style" "fill:#343434"))
-    (-> g (.selectAll "text") (.data #js ["prod"]) .enter
-                    (.append "text") (.text (fn [d i] d)) (.attr "dy" 15) (.attr "dx" 30))
-    (-> g (.selectAll "line")
-                  (.data (clj->js [{:x1 30 :x2 30 :y1 34 :y2 103 :class "solid"} {:x1 30 :x2 30 :y1 125 :y2 153 :class "solid"}]))
-                  .enter (.append "line")
-                  (.attr "x1" (fn [d _] (.-x1 d)))
-                  (.attr "x2" (fn [d _] (.-x2 d)))
-                  (.attr "y1" (fn [d _] (.-y1 d)))
-                  (.attr "y2" (fn [d _] (.-y2 d)))
-                  (.attr "class" (fn [d _] (.-class d))))))
-
+(defn create-env-static [parent-element env x-offset]
+  (let [g (create-element-grouping parent-element env x-offset 100)]
+    (create-element-join g "circle" [{:cx 30 :cy 10 :r 24}])
+    (create-text-join g [{:text env :dx 30 :dy 15}])
+    (create-element-join g "line" [{:x1 30 :x2 30 :y1 34 :y2 103 :class "solid"}
+                                   {:x1 30 :x2 30 :y1 125 :y2 153 :class "solid"}])))
 
 (defcomponent slate-3 [{:keys [step] :as cursor} owner]
-  (init-state [_]
-    {:width 1000
-     :height 400})
   (did-mount [_]
     (.log js/console "Slate 3 mounted with step: " step)
     (let [svg (create-svg)]
-      (create-service-line svg)
-      (create-dev-line svg)
-      (create-qa-line svg)
-      (create-prod-line svg)
+      (create-service-static svg "service a" 200)
+      (create-env-static svg "dev" 300)
+      (create-env-static svg "qa" 600)
+      (create-env-static svg "prod" 900)
       (om/set-state! owner :svg svg)))
   (did-update [_ _ _]
     (.log js/console "Slate 3 updated with step: " step)
