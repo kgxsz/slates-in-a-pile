51495ef2cd5ba38e4e8d5a0396faaf4532c5ed7c
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-06-21 16:06:53 +0100

    Tiny formatting fix

--- src/slates_in_a_pile/styles/utils/helpers.clj
+++ src/slates_in_a_pile/styles/utils/helpers.clj
@@ -1,10 +1,9 @@
 (ns slates-in-a-pile.styles.utils.helpers)
 
 (defn nth-child [& styles]
-  (->>
-    (partition-by string? styles)
-    (partition-all 2)
-    (map (fn [[h t]]
-           (let [[n & styles] (concat h t)]
-             [(format "&:nth-child(%s)" n)
-              styles])))))
+  (->> (partition-by string? styles)
+       (partition-all 2)
+       (map (fn [[h t]]
+              (let [[n & styles] (concat h t)]
+                [(format "&:nth-child(%s)" n)
+                 styles])))))
